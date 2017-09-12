3cbc1031427391da52c0e8c3248a8cbb919e859d
Jose Villegas <jfv33@cornell.edu>
2017-09-11 20:17:16 -0400

    Added notes as comments about Tight Coupling and Low Cohesion

--- examples/Lecture02/app.js
+++ examples/Lecture02/app.js
@@ -1,10 +1,18 @@
+// object literal
 var student = {
   name: "",
   type: "student"
 };
 
+// Set up some listeners for different events
+
+// Fire the contentLoaded function when the entire page
+// is loaded and is intantiated inside the browser's memory
 document.addEventListener('DOMContentLoaded', contentLoaded);
 
+// Set up a keyup listener that fires the keyUp function
+// when there is a keyup in the element whose id is 'name'
+// (i.e. the input text box).
 function contentLoaded(event) {
   document.getElementById('name').addEventListener("keyup", keyUp);
 }
@@ -22,6 +30,11 @@ function calculateNumericOutput() {
   }
 
   // Insert result into page
+
+  // Low Cohesion: this function is doing too much.
+  /// It should just be computing totalNameValue,
+  // not also builidng the string that is presented to the user.
+  // The text should be moved to the presentation code, i.e. to index.html.
   var output = "Total Numeric value of person's name is " + totalNameValue;
   document.getElementById('output').innerText = output;
 }
